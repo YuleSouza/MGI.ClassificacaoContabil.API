876bf2e1a254115c6fe6671af0e97075ac5315fb
devAn <andretdswork@gmail.com>
2024-09-30 15:03:59 -0300

    Ajuste consulta combo de projeto

--- MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/DTO/Projeto/ProjetoDTO.cs
+++ MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/DTO/Projeto/ProjetoDTO.cs
@@ -5,7 +5,7 @@ namespace Service.DTO.Projeto
     public class ProjetoDTO
     {
         public int CodProjeto { get; set; }
-        public string? Nome { get; set; }
+        public string? NomeProjeto { get; set; }
         public int IdClassifContabil { get; set; }
         public LancamentoContabilDTO? Lancamentos { get; set; }
         public IEnumerable<FaseContabilDTO>? Fase { get; set; }
