c65840535fe850bc2357a24e7d7c612320cca373
devAn <andretdswork@gmail.com>
2024-11-05 22:24:56 -0300

    Refatoração crud cenário

--- MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/Service/Cenario/CenarioService.cs
+++ MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/Service/Cenario/CenarioService.cs
@@ -1,5 +1,5 @@
 ﻿using DTO.Payload;
-using Infra.Interface;
+using MGI.ClassificacaoContabil.Service.Helper;
 using Service.DTO.Cenario;
 using Service.DTO.Filtros;
 using Service.Interface.Cenario;
@@ -10,48 +10,26 @@ namespace Service.Cenario
     public class CenarioService : ICenarioService
     {
         private ICenarioRepository _repository;
-        private IUnitOfWork _unitOfWork;
+        private ITransactionHelper _transactionHelper;
 
-        public CenarioService(ICenarioRepository cenarioRepository, IUnitOfWork unitOfWork)
+        public CenarioService(ICenarioRepository cenarioRepository, ITransactionHelper transactionHelper)
         {
             _repository = cenarioRepository;
-            _unitOfWork = unitOfWork;
+            _transactionHelper = transactionHelper;
         }
         public async Task<PayloadDTO> InserirCenario(CenarioDTO cenario)
         {
-            using (IUnitOfWork unitOfWork = _unitOfWork)
-            {
-                try
-                {
-                    unitOfWork.BeginTransaction();
-                    bool ok = await _repository.InserirCenario(cenario);
-                    unitOfWork.Commit();
-                    return new PayloadDTO("Cenário inserido com successo", ok, string.Empty);
-                }
-                catch (Exception ex)
-                {
-                    unitOfWork.Rollback();
-                    return new PayloadDTO("Erro ao inserir Cenário", false, ex.Message);
-                }
-            }
+            return await _transactionHelper.ExecuteInTransactionAsync(
+                async () => await _repository.AlterarCenario(cenario),
+                "Cenário criado com successo"
+            );
         }
         public async Task<PayloadDTO> AlterarCenario(CenarioDTO cenario)
         {
-            using (IUnitOfWork unitOfWork = _unitOfWork)
-            {
-                try
-                {
-                    unitOfWork.BeginTransaction();
-                    bool ok = await _repository.AlterarCenario(cenario);
-                    unitOfWork.Commit();
-                    return new PayloadDTO("Cenário alterado com successo", ok, string.Empty);
-                }
-                catch (Exception ex)
-                {
-                    unitOfWork.Rollback();
-                    return new PayloadDTO("Erro na alteração Cenário", false, ex.Message);
-                }
-            }
+            return await _transactionHelper.ExecuteInTransactionAsync(
+                async () => await _repository.AlterarCenario(cenario),
+                "Cenário alterado com successo"
+            );
         }
         public async Task<PayloadDTO> ConsultarCenario()
         {
