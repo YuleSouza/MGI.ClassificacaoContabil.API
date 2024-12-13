597765da6739c6fcc38ed74f4f7d1eea001e4aae
devAn <andretdswork@gmail.com>
2024-12-13 00:37:50 -0300

    Ajuste logs das classificações

--- MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/DTO/Esg/Classificacao/AprovacaoClassifEsg.cs
+++ MGI.ClassificacaoContabil.Service/MGI.ClassificacaoContabil.Service/DTO/Esg/Classificacao/AprovacaoClassifEsg.cs
@@ -7,6 +7,39 @@
         public char Aprovacao { get; set; }
         public string UsCriacao { get; set; }
         public DateTime DtCriacao { get; set; }
+        public string MensagemLog
+        {
+            get
+            {
+                string texto = string.Empty;
+                switch (Aprovacao)
+                {
+                    case 'P':
+                        {
+                            texto = $"Criado em {DtCriacao.ToString("dd/MM/yyyy")} por {UsCriacao}";
+                            break;
+                        }
+                    case 'A':
+                        {
+                            texto = $"Aprovado em {DtCriacao.ToString("dd/MM/yyyy")} por {UsCriacao}";
+                            break;
+                        }
+                    case 'R':
+                        {
+                            texto = $"Reprovado em {DtCriacao.ToString("dd/MM/yyyy")} por {UsCriacao}";
+                            break;
+                        }
+                    case 'E':
+                        {
+                            texto = $"Excluído em {DtCriacao.ToString("dd/MM/yyyy")} por {UsCriacao}";
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                return texto;
+            }            
+        }
     }
 
 }
